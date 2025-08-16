5f135bf23e6d33aa4a8d6696fc7871f54ed425bb
daaditya5 <daaditya5@gmail.com>
2025-08-16 15:07:54 +0530

    Added prime_nums_100.py

---
+++ prime_nums_100.py
@@ -0,0 +1,13 @@
+def find_primes(n):
+    primes = []
+    for i in range(2, n):
+        is_prime = True
+        for j in range(2, int(i**0.5) + 1):
+            if i % j == 0:
+                is_prime = False
+                break
+        if is_prime:
+            primes.append(i)
+    return primes
+
+print(find_primes(100))
